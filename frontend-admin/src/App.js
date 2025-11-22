27e61aaf3e5852ead4f04ebf2bc5090e3f6fe2b5
Jacobo <jacobo@Mac-mini-de-Jacobo.local>
2025-11-21 19:26:22 -0600

    Se repara bug de enrutamiento en el admin

--- frontend-admin/src/App.js
+++ frontend-admin/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import AdminMenu from "./components/AdminMenu";
 import Login from "./components/Login";
 import RegisterUser from "./components/RegisterUser";
